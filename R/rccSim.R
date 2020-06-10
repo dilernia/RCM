32312b61b7e0dac24e3f221f49adbe1d0e918af7
Andrew DiLernia <andrew.s.dilernia@gmail.com>
2020-06-10 10:24:30 -0500

    Update rccSim.R
    (1) Added eprob parameter

--- R/rccSim.R
+++ R/rccSim.R
@@ -28,6 +28,7 @@
 #' @param overlap Positive number between 0 and 1. Approximate proportion of overlapping edges across cluster-level networks.
 #' @param rho Positive number between 0 and 1. Approximate proportion of differential edges for subjects compared to their corresponding cluster-level network.
 #' @param type Graph type. Options are "hub" or "random".
+#' @param eprob Probability of two nodes having an edge between them. Only applicable if type = "random".
 #' @return A list of length 5 containing:
 #' \enumerate{
 #' \item list of \eqn{K} multivariate-Gaussian data sets each of dimension \eqn{n_k} x \eqn{p} (simDat).
@@ -52,7 +53,7 @@
 #'
 #' @export
 rccSim <- function(G = 2, clustSize = c(67, 37), p = 10,
-                   n = 177, overlap = 0.50, rho = 0.10, type = "hub") {
+                   n = 177, overlap = 0.50, rho = 0.10, type = "hub", eprob = 0.50) {
 
   # Calculating total number of subjects
   K <- ifelse(length(clustSize) == 1, G * clustSize, sum(clustSize))
